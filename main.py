63b7eedf8d0f15017a943aae1d85c759c98fcea7
Brendan Devlin-Hill <brendandevlin-hill@compscilap113.cs.york.ac.uk>
2023-09-05 15:03:49 +0100

    Grid test test is now working

--- main.py
+++ main.py
@@ -28,140 +28,79 @@ intQ = pd.read_pickle(data_directory + "rects_id12_integratedQ.pkl")
 # each column corresponds to the quality score of the actions
 # actions are wait/move cw/move ccw/inspect
 
-# generate all states.
-
-num_segments = 10
-max_segment = num_segments-1
-
-states = []
-states_dict = {}
-counter = 0
-
-damaged_segments = [1,5,9]
-
 #%%
 
 print("Generating states...")
 
-for r1_pos in range(0, num_segments):
-    for r2_pos in range(0, num_segments):
-        for r3_pos in range(0,num_segments):
-            
-            for r1_insp in [0,1]:
-                for r2_insp in [0,1]:
-                    for r3_insp in [0,1]:
-                        
-                        for d1 in [-1, damaged_segments[0]]:
-                            for d2 in [-1, damaged_segments[1]]:
-                                for d3 in [-1, damaged_segments[2]]:
+# generate a 2d grid
+grid_size = 4
+goal_position = np.array([[3,3]])
 
-                                    state = np.array([r1_pos, r2_pos, r3_pos, r1_insp, r2_insp, r3_insp, d1, d2, d3])
-                                    states.append(state)
-                                    states_dict[str(state)] = counter
-                                    counter+=1
-                        
+states = []
 
-np.savetxt(os.getcwd() + "/states.txt", states)
-
-#%%
-# define possible state transitions. [state, state, state, state...]
-# the row index corresponds to the outgoing state
-# the column index corresponds to the action taken
-# the contents of [i,j] corresponds to the index of the resultant state
-
-
-
-transitions = np.zeros((len(states),9))
-transition_rewards = np.zeros((len(states),9))
-
-print("Generating transitions and rewards...")
-
-for i in range(len(states)):
-    
-    if (i%1000==0):
-        print("Transitions from state {: >5g}/{:g} ({: >4.2f}%)".format(i, len(states), i*100/len(states)))
-    
-    # get the state of the system
-    r1_pos, r2_pos, r3_pos, r1_insp, r2_insp, r3_insp, *d_segs = states[i]
-    
-    # array of states that can be accessed from current state index
-    available_states = np.ones(9)*-1
-    t_rewards = np.zeros(9)
-    
-    # nothing happens
-    #available_states[-1] = states_dict[str(states[i])]
-    
-    # robot 1 actions:
-    if (r1_pos<max_segment and not r1_insp):
-        available_states[0] = states_dict[str(np.hstack(([r1_pos+1, r2_pos, r3_pos, r1_insp, r2_insp, r3_insp], d_segs)))]
-        t_rewards[0] = 0
-    if (r1_pos>0 and not r1_insp):    
-        available_states[1] = states_dict[str(np.hstack(([r1_pos-1, r2_pos, r3_pos, r1_insp, r2_insp, r3_insp], d_segs)))]
-        t_rewards[1] = 0
-    if (r1_pos in damaged_segments or r1_insp==1):
-        ds = [-1 if (a==r1_pos) else a for a in d_segs]
-        available_states[2] = states_dict[str(np.hstack(([r1_pos, r2_pos, r3_pos, abs(r1_insp-1), r2_insp, r3_insp], ds)))]
-        t_rewards[2] = len(np.argwhere(np.array(ds) < 0))**2
-    
-    # robot 2 actions:
-    if (r2_pos<max_segment and not r2_insp):
-        available_states[3] = states_dict[str(np.hstack(([r1_pos, r2_pos+1, r3_pos, r1_insp, r2_insp, r3_insp], d_segs)))]
-        t_rewards[3] = 0
-    if (r2_pos>0 and not r2_insp):
-        available_states[4] = states_dict[str(np.hstack(([r1_pos, r2_pos-1, r3_pos, r1_insp, r2_insp, r3_insp], d_segs)))]
-        t_rewards[4] = 0
-    if (r2_pos in damaged_segments or r2_insp==1):
-        ds = [-1 if (a==r2_pos) else a for a in d_segs]
-        available_states[5] = states_dict[str(np.hstack(([r1_pos, r2_pos, r3_pos, r1_insp, abs(r2_insp-1), r3_insp], ds)))]
-        t_rewards[5] = len(np.argwhere(np.array(ds) < 0))**2
-
-    # robot 3 actions:
-    if (r3_pos<max_segment and not r3_insp):
-        available_states[6] = states_dict[str(np.hstack(([r1_pos, r2_pos, r3_pos+1, r1_insp, r2_insp, r3_insp], d_segs)))]
-        t_rewards[6] = 0
-    if (r3_pos>0 and not r3_insp):
-        available_states[7] = states_dict[str(np.hstack(([r1_pos, r2_pos, r3_pos-1, r1_insp, r2_insp, r3_insp], d_segs)))]
-        t_rewards[7] = 0
-    if (r3_pos in damaged_segments or r3_insp==1):
-        ds = [-1 if (a==r3_pos) else a for a in d_segs]
-        available_states[8] = states_dict[str(np.hstack(([r1_pos, r2_pos, r3_pos, r1_insp, r2_insp, abs(r3_insp-1)], ds)))]
-        t_rewards[8] = 1 if any((np.array(ds) - np.array(d_segs)) < 0) else 0
-
-    transitions[i] = available_states
-    transition_rewards[i] = t_rewards        
-    
-print("\rSaving transitions to " + os.getcwd() + f"/transitions.txt")
-np.savetxt(os.getcwd() + f"/transitions.txt", transitions)
+for i in range(0, grid_size):
+    for j in range(0, grid_size):
+        states.append([i,j])
 
 states = np.array(states)
 
 #%%
 
-# now for the learning...
+# define possible state transitions. [state, state, state, state...]
+# the row index corresponds to the outgoing state
+# the column index corresponds to the action taken
+# the contents of [i,j] corresponds to the index of the resultant state
 
+def isAinB(a,b):
+    for entry in b:
+        print(entry, a)
+        if entry[0] == a[0] and entry[1] == a[1]:
+            return True
+    return False
+            
 
-initial_state_indices = []
-num_segments = 10
+transitions = np.zeros((len(states),4,2))
+transition_rewards = np.zeros((len(states),4))
 
+print("Generating transitions and rewards...")
 
-for r1_pos in range(0, num_segments):
-    for r2_pos in range(0, num_segments):
-        for r3_pos in range(0,num_segments):
-                        state = np.hstack(([r1_pos, r2_pos, r3_pos, 0, 0, 0], damaged_segments))
-                        initial_state_indices.append(states_dict[str(state)])
-
-
-print(f"Number of initial states: {len(initial_state_indices)}")
-
+for i in range(len(states)):
+    
+    available_states = np.ones((4,2))*-1
+    available_rewards = np.zeros(4)
+    
+    if states[i,0]>0: #left 
+        new_state = states[i] - [1,0]
+        available_states[0] = new_state
+        if isAinB(new_state, goal_position):
+            available_rewards[0] = 1
+        
+    if states[i,0]<(grid_size-1): #right 
+        new_state = states[i] + [1,0]
+        available_states[1] = new_state
+        if isAinB(new_state, goal_position):
+            available_rewards[1] = 1
+        
+    if states[i,1]>0: #up 
+        new_state = states[i] - [0,1]
+        available_states[2] = new_state
+        if isAinB(new_state, goal_position):
+            available_rewards[2] = 1
+        
+    if states[i,1]<(grid_size-1): #down 
+        new_state = states[i] + [0,1]
+        available_states[3] = new_state
+        if isAinB(new_state, goal_position):
+            available_rewards[3] = 1
+        
+    transitions[i] = available_states
+    transition_rewards[i] = available_rewards
+    
+print(transitions)
+    
 #%%
 
-# agent = np.loadtxt(os.getcwd() + "/agent.txt")
-# transitions = np.loadtxt(os.getcwd() + "/transitions.txt")
-# states = np.loadtxt(os.getcwd() +  "/states.txt")
-
-#%%
-
-Qtable = np.random.rand(*np.shape(transitions))
+Qtable = np.random.rand(len(states),4)
 
 def AverageDistancePseudReward(system, state, action):
     r_pos, _, d_seg = system.interpret_state(state)
@@ -172,6 +111,9 @@ def AverageDistancePseudReward(system, state, action):
             rob_av += abs(r_pos[i] - d_seg[j])/len(d_seg)
         tot_av += rob_av/len(r_pos)
     return 10/tot_av
+
+def EmptyPseudoReward(system, state, action):
+    return 0
 
 agent = ql.Train(Qtable, 
               ql.PrimitiveSystem([1,5,9], states, transitions, transition_rewards, AverageDistancePseudReward),
