ed88b02d4f5d09e753962cd98b9c4a196a7a97a4
Brendan Devlin-Hill <brendandevlin-hill@compscilap113.cs.york.ac.uk>
2024-03-06 14:37:54 +0000

    Added another layer to the DQN, which allows it to easily converge for 5 goals.

--- DQN.py
+++ DQN.py
@@ -77,9 +77,11 @@ class DeepQNetwork(nn.Module):
 
     def __init__(self, n_observations, n_actions):
         super(DeepQNetwork, self).__init__()
-        self.layer1 = nn.Linear(n_observations, 128)
-        self.layer2 = nn.Linear(128, 128)
-        self.layer3 = nn.Linear(128, n_actions)
+        self.input_layer = nn.Linear(n_observations, 128)
+        self.hidden_layer1 = nn.Linear(128, 128)
+        self.hidden_layer2 = nn.Linear(128, 128)
+        self.hidden_layer3 = nn.Linear(128, 128)
+        self.ouput_layer = nn.Linear(128, n_actions)
 
     # Called with either one element to determine next action, or a batch
     # during optimization. Returns tensor([[left0exp,right0exp]...]).
