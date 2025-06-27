a93fb0383ab4e9bc45db0c15c7e2cca779ca4097
BrendanDH <brendan.devlin-hill@york.ac.uk>
2025-06-27 20:52:46 +0100

    Sync

--- system_logic/engage_MA.py
+++ system_logic/engage_MA.py
@@ -277,7 +277,7 @@ def r_model(env, old_state_dict,robot_no, action_no, next_state_dict):
             if (old_state_dict[f"goal{i} location"] == old_state_dict[f"robot{robot_no} location"] and
                 old_state_dict[f"goal{i} active"] == 1):
                 # prob = old_state_dict[f"goal{i} completion probability"]
-                reward = 0.075 * old_state_dict[f"robot{robot_no} engagement"]
+                reward = 0.005 * old_state_dict[f"robot{robot_no} engagement"]
                 
     # # reward for having moved into a terminal state
     if (state_is_final(env, next_state_dict)):
