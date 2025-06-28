35a1b2b6f902066a7f09f7ccfa9ad4027e87d227
BrendanDH <brendan.devlin-hill@york.ac.uk>
2025-06-28 17:01:23 +0100

    Sync

--- system_logic/engage_MA.py
+++ system_logic/engage_MA.py
@@ -244,9 +244,9 @@ def r_model(env, old_state_dict,robot_no, action_no, next_state_dict):
         other_robot_loc = next_state_dict[f"robot{i} location"]
         robot_dist = abs(moving_robot_loc - other_robot_loc)  # non-mod distance
         mod_robot_dist = min(robot_dist, env.unwrapped.size - robot_dist)
-        if (mod_robot_dist < 2):
+        if (mod_robot_dist == 1):
             reward -= 0.07 * old_state_dict[f"robot{i} engagement"]
-            print(f"{robot_no} displaces {i}, reward {reward} ")
+            # print(f"{robot_no} displaces {i}, reward {reward} ")
             
     # reward for checking a goal by moving onto its position
     if (action_no == 0 or action_no == 1):
